c13b89bcd603fff77c0bb3c06ac25773ced42259
Aiken Ong <aiken.ong@u.nus.edu>
2024-11-22 13:02:00 +0800

    Initial commit

---
+++ src/main/java/personal/blogplatform/BlogplatformApplication.java
@@ -0,0 +1,13 @@
+package personal.blogplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogplatformApplication.class, args);
+	}
+
+}
